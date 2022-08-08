da7b1f485244ecf31fbf28711af420a72766f8f2
Pignuuu <Pignuuu@gmail.com>
2022-08-08 21:08:24 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -2,52 +2,29 @@
   <div v-if="downloading !== true" class="page">
     <h2>Enter twitch vod URL or ID here</h2>
     <input type="text" v-model="url" />
-    <button v-if="this.url !== ''" @click="getVod">Get Vod</button>
+    <button v-if="this.url && this.show !== ''" @click="getVod">Get Vod</button>
   </div>
   <div v-else-if="this.finished == true" class="page">
-    <h2>Vod finished downloading</h2>
-    <button @click="this.download">Download</button>
-    <h3>
-      Download should automatically start but it takes a while if VOD is long.
-    </h3>
-    <h3>But if download still doesn't start press this sucker.</h3>
+    <h2>Please wait while VOD is downloaded from the server.</h2>
+    <LoadingSpinner2 />
+    <br />
+    <div v-if="this.downloadButton == true">
+      <h3>
+        Download is taking a while, if download doesn't start press this button.
+      </h3>
+      <button @click="this.download">Download</button>
+    </div>
   </div>
   <div v-else class="page">
-    <h2>Downloading Vod {{ this.vodId }}</h2>
-    <div class="car">
-      {{ this.loader1 }}
-      <br />
-      {{ this.loader2 }}
-      <br />
-      {{ this.loader3 }}
-      <br />
-      {{ this.loader4 }}
-      <br />
-    </div>
-    <h3>Depending on the length of the VOD this can take a few minutes.</h3>
-    <h3>
-      Also please do not close the window as then you will have to restart
-      (╯°□°）╯︵ ┻━┻
-    </h3>
-  </div>
-  <div class="socials">
-    You can find ways to contact me on my website.
-    <a style="color: #42b983" href="https://stianwiu.me" target="_blank"
-      >https://stianwiu.me</a
-    >
-    <br />
-    <br />
-    And thanks for using my free service. Hope you find it useful.
-    <br />
-    <br />
-    Consider donating if you like it.
-    <a style="color: #42b983" href="https://ko-fi.com/stianwiu" target="_blank"
-      >https://ko-fi.com/stianwiu</a
-    >
+    <h2>Please wait while we download the vod to the server</h2>
+    <h2>Vod ID: {{ this.vodId }}</h2>
+    <LoadingSpinner></LoadingSpinner>
   </div>
 </template>
 
 <script>
+import LoadingSpinner from "./components/LoadingSpinner.vue";
+import LoadingSpinner2 from "./components/LoadingSpinner2.vue";
 const axios = require("axios");
 export default {
   name: "App",
@@ -57,15 +34,12 @@ export default {
       downloading: false,
       finished: false,
       vodId: "",
-      loader1: "",
-      loader2: "",
-      loader3: "",
-      loader4: "",
-      loaderVersion: 0,
+      show: "1",
     };
   },
   methods: {
     async getVod() {
+      this.show = "";
       // Keep only numbers from string
       this.vodId = this.url.match(/[0-9]+/g).join("");
       await axios
@@ -93,50 +67,6 @@ export default {
               })
               .then((response) => {
                 console.log(response);
-                if (this.loaderVersion == 0) {
-                  this.loader1 = "ㅤㅤㅤㅤ______";
-                  this.loader2 = "ㅤㅤㅤ/|_||_\\`.__";
-                  this.loader3 = "ㅤㅤ(ㅤㅤ_ㅤㅤ_ㅤ_\\";
-                  this.loader4 = "ㅤㅤ=`-(_)--(_)-'";
-                  this.loaderVersion++;
-                } else if (this.loaderVersion == 1) {
-                  this.loader1 = "ㅤㅤㅤㅤㅤㅤ______";
-                  this.loader2 = "ㅤㅤㅤㅤㅤ/|_||_\\`.__";
-                  this.loader3 = "ㅤㅤㅤㅤ(ㅤㅤ_ㅤㅤ_ㅤ_\\";
-                  this.loader4 = "ㅤㅤㅤㅤ=`-(_)--(_)-'";
-                  this.loaderVersion++;
-                } else if (this.loaderVersion == 2) {
-                  this.loader1 = "ㅤㅤㅤㅤㅤㅤㅤㅤ______";
-                  this.loader2 = "ㅤㅤㅤㅤㅤㅤㅤ/|_||_\\`.__";
-                  this.loader3 = "ㅤㅤㅤㅤㅤㅤ(ㅤㅤ_ㅤㅤ_ㅤ_\\";
-                  this.loader4 = "ㅤㅤㅤㅤㅤㅤ=`-(_)--(_)-'";
-                  this.loaderVersion++;
-                } else if (this.loaderVersion == 3) {
-                  this.loader1 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ______";
-                  this.loader2 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤ/|_||_\\`.__";
-                  this.loader3 = "ㅤㅤㅤㅤㅤㅤㅤㅤ(ㅤㅤ_ㅤㅤ_ㅤ_\\";
-                  this.loader4 = "ㅤㅤㅤㅤㅤㅤㅤㅤ=`-(_)--(_)-'";
-                  this.loaderVersion++;
-                } else if (this.loaderVersion == 4) {
-                  this.loader1 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ______";
-                  this.loader2 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ/|_||_\\`.__";
-                  this.loader3 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ(ㅤㅤ_ㅤㅤ_ㅤ_\\";
-                  this.loader4 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ=`-(_)--(_)-'";
-                  this.loaderVersion++;
-                } else if (this.loaderVersion == 5) {
-                  this.loader1 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ______";
-                  this.loader2 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ/|_||_\\`.__";
-                  this.loader3 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ(ㅤㅤ_ㅤㅤ_ㅤ_\\";
-                  this.loader4 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ=`-(_)--(_)-'";
-                  this.loaderVersion++;
-                } else if (this.loaderVersion == 6) {
-                  this.loader1 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ______";
-                  this.loader2 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ/|_||_\\`.__";
-                  this.loader3 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ(ㅤㅤ_ㅤㅤ_ㅤ_\\";
-                  this.loader4 = "ㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤㅤ=`-(_)--(_)-'";
-                  this.loaderVersion = 0;
-                }
-
                 if (response.data == true) {
                   this.finished = true;
                   this.download();
@@ -157,15 +87,23 @@ export default {
       }).then((response) => {
         var fileURL = window.URL.createObjectURL(new Blob([response.data]));
         var fURL = document.createElement("a");
-
         fURL.href = fileURL;
         fURL.setAttribute("download", `${this.vodId} - vod.astraea.dev.mp4`);
         document.body.appendChild(fURL);
-
         fURL.click();
+
+        this.url = "";
+        this.downloading = false;
+        this.finished = false;
+        this.vodId = "";
+        this.show = "1";
       });
+      // Wait for 2 minutes
+      await new Promise((resolve) => setTimeout(resolve, 120000));
+      this.downloadButton = true;
     },
   },
+  components: { LoadingSpinner, LoadingSpinner2 },
 };
 </script>
 
@@ -221,10 +159,6 @@ button:hover {
   justify-content: center;
   align-items: center;
   align-content: stretch;
-}
-
-.car {
-  text-align: initial;
 }
 
 .socials {
---
+++ src/components/LoadingSpinner2.vue
@@ -0,0 +1,62 @@
+<template>
+  <span class="loader"></span>
+</template>
+
+<script>
+export default {
+  name: "LoadingSpinner2",
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.loader {
+  width: 48px;
+  height: 48px;
+  display: inline-block;
+  position: relative;
+  border: 2px solid #e4f5ef;
+  box-sizing: border-box;
+  animation: rotation 2s linear infinite;
+}
+.loader::after,
+.loader::before {
+  content: "";
+  box-sizing: border-box;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  margin: auto;
+  border: 2px solid #42b983;
+  width: 38px;
+  height: 38px;
+  animation: rotationBack 1.5s linear infinite;
+  transform-origin: center center;
+}
+.loader::before {
+  width: 28px;
+  height: 28px;
+  border-color: #e4f5ef;
+  box-sizing: border-box;
+  animation: rotation 1s linear infinite;
+}
+
+@keyframes rotation {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+@keyframes rotationBack {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(-360deg);
+  }
+}
+</style>
